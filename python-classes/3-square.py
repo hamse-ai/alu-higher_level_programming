3fde9b5a0680e3cc19a92b42aabf795346f200b5
Hamse <h.jama@alustudent.com>
2024-11-07 16:50:31 +0200

    3-square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""a class for a Square with size and are"""
+"""a class for a Square with size and area"""
 
 
 class Square:
     """Square class represents a square with size and area."""
 
     def __init__(self, size=0):
-        """Initialize the square with sizenand area.
+        """Initialize the square with size.
 
         Args:
             size (int): The size of the square.
@@ -20,6 +20,11 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    def area(self, size):
-        """Calculate the area of the square."""
-        return size ** 2
+
+    def area(self):
+        """Calculate and return the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2

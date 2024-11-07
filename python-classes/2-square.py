be22eba313b643287670cd4b11eddb1c54a85e82
Hamse <h.jama@alustudent.com>
2024-11-07 16:38:42 +0200

    2-square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""a class for a Square" wwith size"""
+"""a class for a Square with size"""
 
 
 class Square:
     """Square class represents a square with size."""
-    def __init__(self, size):
-        """Initialize the square with size."""
+
+    def __init__(self, size=0):
+        """Initialize the square with size.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If `size` is not an integer.
+            ValueError: If `size` is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
-        try:
-            type(size) == int
-        except TypeError: 
-            print("size must be an intege")
-        except ValueError:
-            print("size must be >= 0")
-    
-    pass

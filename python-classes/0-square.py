5b941805a8d9a462c04b01c96650f16e6f777fa9
Hamse <h.jama@alustudent.com>
2024-11-07 16:16:53 +0200

    document

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """an empty class for"""
+
 class square:
-    """
-    Square class represents an empty square.
-    """
+    """Square class represents an empty square."""
     pass

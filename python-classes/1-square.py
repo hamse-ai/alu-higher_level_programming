0ccfe9834f3d025ea6abde771dd63eb73ff5384f
Hamse <h.jama@alustudent.com>
2024-11-07 16:26:25 +0200

    -1square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class for a Square" wwith size"""
+
+
+class Square:
+    """Square class represents a square with size."""
+    def __init__(self, size):
+        """Initialize the square with size."""
+        self.__size = size
+    pass

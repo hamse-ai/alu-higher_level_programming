9c28b9b66be19c62704765fdb9dc1b34d67b2849
Hamse <h.jama@alustudent.com>
2024-11-30 12:00:16 +0200

    task 3

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# are you well commented
-curl -sI "$1" | grep "Allow: " | cut -d " " -f 2-
+# takes in a URL and displays all HTTP methods the server will accept
+curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
